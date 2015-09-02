40f14742eab05626842568e2ca9b84d53c9b28ae
dixonjamesa <dixonjamesa@gmail.com>
2015-09-02 16:32:25 +0100

    FFT drawn twice; added envelope

--- WpfApplication1/AudioAnalyser.cs
+++ WpfApplication1/AudioAnalyser.cs
@@ -24,9 +24,10 @@ namespace WpfApplication1
         public BandpassControl bpc { get; set; }
         public int Fundamental {  get { return fft.fundamentalFrequency;  } }
         WriteableBitmap Wavemap;
+        WriteableBitmap FFTmap;
         public int CrossfadeSampleSize;
         public double GraphScale = 1.0;
-        double[] CrossfadeBuffer;
+        byte[] Clear;
 
 
         public AudioAnalyser(IWaveProvider source, Image image, Image image2)
@@ -36,7 +37,9 @@ namespace WpfApplication1
             FFTImage = image;
             WaveImage = image2;
             CrossfadeSampleSize = 20;
-            CrossfadeBuffer = new double[200];
+            FFTmap = new WriteableBitmap((int)image.Width, (int)image.Height, 96, 96, PixelFormats.Bgr32, null);
+            Clear = new byte[(int)image.Width * (int)image.Height * 4];
+            FFTmap.CopyPixels(Clear, (int)image.Width * 4, 0);
         }
         public WaveFormat WaveFormat
         {
@@ -61,7 +64,7 @@ namespace WpfApplication1
             
             Wavemap = new WriteableBitmap((int)WaveImage.Width, (int)WaveImage.Height, 96, 96, PixelFormats.Bgr32, null);
             WaveImage.Source = Wavemap;
-
+            FFTmap.WritePixels(new Int32Rect(0, 0, (int)FFTmap.Width, (int)FFTmap.Height), Clear, (int)FFTmap.Width * 4, 0);
             //DrawWaveform(WaveImage, waveBuffer.ShortBuffer, samples,200,100,50);
             fft.Convert1DData(waveBuffer.ShortBuffer, samples, bpc.WindowSize);
             DrawWaveform(WaveImage, fft.vector, samples, 200, 100, 50);
@@ -70,7 +73,7 @@ namespace WpfApplication1
             {
                 fft.ComplexFFT(1);
 
-                DrawFFT(FFTImage, fft);
+                //DrawFFT(FFTImage, fft, 50, 100, 200);
                 fft.vector[0] = fft.vector[1] = 0;
                 for (int i = fft.vector.Length / 2; i < fft.vector.Length; i++)
                 {
@@ -98,6 +101,7 @@ namespace WpfApplication1
                     fft.vector[i * 2] = 0;
                     fft.vector[i * 2 + 1] = 0;
                 }
+                //DrawFFT(FFTImage, fft, 200, 100, 50);
 #endif
                 fft.ComplexFFT(-1);
             }
@@ -105,42 +109,41 @@ namespace WpfApplication1
             // 
             for (int i = 0; i < CrossfadeSampleSize; i++)
             {
-                double source = CrossfadeBuffer[i];
                 double target = fft.vector[(i % bpc.WindowSize) * 2];
-                double proptarget = ((double)i) / CrossfadeSampleSize;
-                double propsource = 1.0 - proptarget;
-                double crossfade = source * propsource + target * proptarget;
-                short val = (short)(crossfade * 32768f);
+                double prop = ((double)i) / CrossfadeSampleSize;
+                short val = (short)(target*prop * 32768f);
                 outBuffer.ShortBuffer[i] = (short)(val * bpc.Gain);
             }
             // 
-            for (int i = CrossfadeSampleSize; i < samples; i++)
+            for (int i = CrossfadeSampleSize; i < samples - CrossfadeSampleSize; i++)
             {
                 double dv = fft.vector[(i % bpc.WindowSize) * 2];
                 short val = (short)(dv*32768f);
                 outBuffer.ShortBuffer[i] = (short)(val * bpc.Gain);
             }
             // 
-            for (int i = samples; i < samples+CrossfadeSampleSize; i++)
+            for (int i = samples - CrossfadeSampleSize; i < samples; i++)
             {
-                double dv = fft.vector[((samples-1) % bpc.WindowSize) * 2];
-                CrossfadeBuffer[i - samples] = dv;
+                double target = fft.vector[(i % bpc.WindowSize) * 2];
+                double prop = 1.0 - ((double)i - (samples-CrossfadeSampleSize)) / CrossfadeSampleSize;
+                short val = (short)(target * prop * 32768f);
+                outBuffer.ShortBuffer[i] = (short)(val * bpc.Gain);
             }
             //DrawWaveform(WaveImage, fft.vector, samples, 50, 100, 200);
             DrawWaveform(WaveImage, outBuffer.ShortBuffer, samples,50,200,100);
             return count;
         }
 
-        void DrawFFT(Image target, FFT fft)
+        void DrawFFT(Image target, FFT fft, byte r, byte g, byte b)
         {
-            WriteableBitmap bitmap = new WriteableBitmap((int)target.Width, (int)target.Height, 96, 96, PixelFormats.Bgr32, null);
+           
 
             byte[] colorData = new byte[(int)target.Height * 4];
             for(int i = 0 ; i< (int)target.Height ; i++)
             {
-                colorData[i * 4 + 0] = 200;
-                colorData[i * 4 + 1] = 100;
-                colorData[i * 4 + 2] = 50;
+                colorData[i * 4 + 0] = b;
+                colorData[i * 4 + 1] = g;
+                colorData[i * 4 + 2] = r;
                 colorData[i * 4 + 3] = 255;
             }
 
@@ -149,9 +152,9 @@ namespace WpfApplication1
                 int index = x * bpc.WindowSize / (int)target.Width / 16;
                 double sample = (Math.Pow(fft.vector[index], 2) + Math.Pow(fft.vector[index + 1], 2)) / fft.fundamentalPower;
                 int y = Math.Min((int)target.Height-1, (int)(sample * target.Height));
-                bitmap.WritePixels(new Int32Rect(x, (int)target.Height-y, 1, y), colorData, 4, 0);
+                FFTmap.WritePixels(new Int32Rect(x, (int)target.Height-y, 1, y), colorData, 4, 0);
             }
-            target.Source = bitmap;
+            target.Source = FFTmap;
         }
         void DrawWaveform(Image target, short[] convertedsamples, int convertedarraysize, byte b, byte g, byte r)
         {
